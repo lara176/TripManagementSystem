04b5a15d91cd69beb373228d16db796efc689297
lara176 <142164180+lara176@users.noreply.github.com>
2026-01-02 16:46:42 +0530

    Update UserController.java

--- src/main/java/com/trips/trip/controller/UserController.java
+++ src/main/java/com/trips/trip/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
